dd05ecff699582d19d4a6ccfee5ba2efe062bd10
Khateeb1111 <junaydkr@hotmail.com>
2022-03-08 17:19:36 +0000

    state

--- src/app.js
+++ src/app.js
@@ -4,9 +4,40 @@ import initialEmails from './data/emails'
 
 import './styles/app.css'
 
+import {useState} from "react"; 
+
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+
+/* Render a list of emails from state so it looks similar to the screenshot above.
+  - You can find an HTML template for each email in the templates folder */
+  const [emails, setEmails]  = useState(initialEmails)
+
+  // Adding Items (3 steps)
+  // DONE 1. Create copy of the original array e.g. emailsCopy is a copy of emails
+  // 2. push the new element into the copy array (.push)
+  // 3. Update the state using setEmails(emailsCopy)
+  
+  const emailsCopy = emails.map(function(emailElement) {
+
+    return <li className="email">
+  <div className="select">
+	<input
+	  className="select-checkbox"
+	  type="checkbox"/>
+  </div>
+  <div className="star">
+	<input
+	  className="star-checkbox"
+	  type="checkbox"
+	/>
+  </div>
+  <div className="sender"> {emailElement.sender} </div>
+  <div className="title"> {emailElement.title} </div>
+</li>
+  })
+
 
   return (
     <div className="app">
@@ -39,7 +70,7 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{emailsCopy}</main>
     </div>
   )
 }
